1cfb44b953ae20ed1cefca25290e61ebc0ab4769
Hoàng Sơn Lâm <115402185+AlRaces@users.noreply.github.com>
2023-03-17 23:12:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,6 @@ import streamlit as st
 
 st.title('aaa')
 temp = np.array(st.number_input('Temp:'))
-temp_pred = model.predict(temp)
+temp_pred = model.predict(temp.reshape(-1, 1))
 if st.button('Giải'):
   st.success(temp_pred)
